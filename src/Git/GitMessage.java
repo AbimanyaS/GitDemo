3e889552f47847c9c9b109cbfa258e6aa8653e63
S <abimanya.s@capgemini.com>
2024-12-17 16:39:56 +0530

    Firt Commit

---
+++ src/Git/GitMessage.java
@@ -0,0 +1,10 @@
+package Git;
+
+public class GitMessage {
+
+	public static void main(String[] args) {
+		System.out.println("Message From STS");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author abimas
+ *
+ */
+module GitDemo {
+}
