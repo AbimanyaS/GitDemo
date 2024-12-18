1ff7bc6dc07f0d6c98a1399995f5203a0141ee19
S <abimanya.s@capgemini.com>
2024-12-18 09:52:41 +0530

    third commit

--- src/Git/GitMessage.java
+++ src/Git/GitMessage.java
@@ -3,8 +3,9 @@ package Git;
 public class GitMessage {
 
 	public static void main(String[] args) {
-		System.out.println("Message From STS");
-		System.out.println("Message From Git");
+		System.out.println("First Message From STS");
+		System.out.println("First Message From Git");
+		System.out.println("Second Message From STS");
 
 	}
 
